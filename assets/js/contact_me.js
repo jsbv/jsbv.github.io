7732fb6f0748cc043331c2cfc4f281a2f7bc6c20
pedro vasconcelos <pnbv@icloud.com>
2016-04-04 11:01:09 +0100

    Update contact_me.js

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/your@email.com",
+                url: "https://formspree.io/abcdefghijklmnopqrstuwxz123456@gmail.com",
                 type: "POST",
                 //data: {
                 //    name: name,
